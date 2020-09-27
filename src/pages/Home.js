562ee3a30cf58611f9d6aeb31d223d32089fd9c3
Dennis Thandy <33387667+dennisthandy@users.noreply.github.com>
2020-09-27 07:56:45 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,31 +4,6 @@ import tailwind from "../tailwindcss.svg";
 
 export const Home = () => {
  return (
-  //   <header className="App-header flex items-center justify-center flex-col">
-  //    <img src={logo} className="w-1/4" alt="logo" />
-  //    <img src={tailwind} className="w-1/6" alt="logo" />
-  //    <p>
-  //     Edit <code>src/App.js</code> and save to reload.
-  //    </p>
-  //    <div className="link mt-5">
-  //     <a
-  //      className="App-link p-2 bg-teal-300 rounded mr-5 hover:bg-teal-400 hover:text-white"
-  //      href="https://reactjs.org"
-  //      target="_blank"
-  //      rel="noopener noreferrer"
-  //     >
-  //      Learn React
-  //     </a>
-  //     <a
-  //      className="App-link p-2 bg-teal-400 rounded hover:bg-teal-600 hover:text-white"
-  //      href="https://tailwindcss.com"
-  //      target="_blank"
-  //      rel="noopener noreferrer"
-  //     >
-  //      Learn Tailwind
-  //     </a>
-  //    </div>
-  //   </header>
   <div className="">
    <div className="flex justify-center items-center">
     <img src={logo} alt="logo" className="w-1/4" />
