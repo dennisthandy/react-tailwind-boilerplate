5c4ad22f4ec6f64a4314609ec396c62b5a8494a9
Dennis Thandy <33387667+dennisthandy@users.noreply.github.com>
2020-09-27 07:56:22 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import { Pages } from "./Pages";
 
 function App() {
